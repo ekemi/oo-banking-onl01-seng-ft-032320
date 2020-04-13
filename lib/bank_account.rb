62cd128c13f9b829d903684ebc4b55056a41b84b
edmundo papi <edmundopierre@yahoo.com>
2020-04-13 04:01:32 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
-class BankAccount
 
-end
+class BankAccount
+    attr_reader :name
+    attr_accessor :balance,:status
+
+    def initialize(bank_name)
+        @name= bank_name
+        @balance = 1000
+        @status = "open"
+    end
+
+
+    def deposit(dep)
+
+      @balance = @balance + dep
+    end
+
+    def display_balance
+
+     "Your balance is $#{@balance}."
+    end
+
+    def valid?
+       valid = (@status =="open"&& @balance > 0)? true : false
+    end
+
+     def close_account
+      @status = "closed"
+     end
+  end
