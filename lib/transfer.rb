b422e3ac9105b4780e6eee3f877d6f887b325d2c
edmundo papi <edmundopierre@yahoo.com>
2020-04-13 04:19:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,41 +1,33 @@
-
 class Transfer
-  attr_accessor :sender, :receiver,:amount, :status
+  attr_accessor :sender, :receiver, :amount, :status
 
-def initialize(sender,receiver,amount)
-
-  #@transfer = transfer
-  @sender = sender
-  @receiver = receiver
-  @status= "pending"
-  @amount = 50
-
-end
-def valid?
-  @receiver.valid? && @sender.valid? ? true : false
-
-end
-
-def execute_transaction
-
-  if @sender.balance > @amount && @status =="pending"
-    @sender.balance -=@amount
-    @receiver.balance +=@amount
-    @status = "complete"
-
-  else
-
-    @status = "rejected"
-   "Transaction rejected.Please check your account again."
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
   end
-end
 
-def reverse_transfer
-if @status =="complete"
-   @sender.balane -=@transfer_amount
-   @receiver.balance+=@transfer_amount
-   @status="reversed"
-end
-end
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
 
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
 end
